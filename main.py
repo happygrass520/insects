1c2fb5a446dd0c272bf9d9cc7b799ad6fcc76985
ztime <jonas@lechuck.se>
2019-11-11 10:44:36 +0100

    Added ability to load perlin values

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import random
 import argparse
+import pickle
 import subprocess
 import tempfile
 import matplotlib.pyplot as plt
@@ -22,78 +23,108 @@ class Insect:
         new_x = current_x + move_x
         new_y = current_y + move_y
         new_z = current_z + move_z
-        if new_x < 0:
-            new_x = 0
-        if new_x >= 128:
-            new_x = 127
-        if new_y < 0:
-            new_y = 0
-        if new_y >= 128:
-            new_y = 127
-        if new_z < 0:
-            new_z = 0
-        if new_z >= 128:
-            new_z = 127
+        if new_x < 0 or new_x >= self.bound:
+            new_x = current_x - move_x
+        # if new_x >= self.bound:
+            # new_x = current_x - move_x
+        # if new_y < 0:
+        if new_y < 0 or new_y >= self.bound:
+            new_y = current_y - move_y
+        # if new_y >= 128:
+            # new_y = 127
+        if new_z < 0 or new_z >= self.bound:
+            new_z = current_z - move_z
+            # new_z = 0
+        # if new_z >= 128:
+            # new_z = 127
         self.position = (new_x, new_y, new_z)
 
 def main():
     # argparse
     parser = argparse.ArgumentParser()
     # parser.add_argument('datafile', type=os.path.abspath)
-    # parser.add_argument('-o', '--output', type=os.path.abspath)
+    parser.add_argument('-o', '--output', type=os.path.abspath)
     # parser.add_argument('-f', '--framerate', type=int, default=25)
     parser.add_argument('--dimX', type=int, default=128)
     parser.add_argument('--dimY', type=int, default=128)
     parser.add_argument('--dimZ', type=int, default=128)
+    parser.add_argument('--perlin_load_path', type=os.path.abspath)
+    parser.add_argument('--perlin_save_path', type=os.path.abspath)
     args = parser.parse_args()
 
-    no_frames = 200
-    frames = generate_grid_with_frames(no_frames, args.dimX, args.dimY, args.dimZ)
+    no_frames = 250
+    box = 256
 
-    # p_x, p_y, p_z = precalculate_values((128,128,128), 256)
     bound = 256
-    p_x, p_y, p_z = precalculate_values((256,256,256))
+    if args.perlin_load_path is None:
+        print("No perlin path, calculating new ones")
+        p_x, p_y, p_z = precalculate_values((256,256,256))
+    else:
+        print(f"Loading perlin from {args.perlin_load_path}...")
+        p_x = load_perlin_noise(args.perlin_load_path, 'p_x')
+        print("Loaded p_x")
+        p_y = load_perlin_noise(args.perlin_load_path, 'p_y')
+        print("Loaded p_y")
+        p_z = load_perlin_noise(args.perlin_load_path, 'p_z')
+        print("Loaded p_z")
+
+    if args.perlin_save_path is not None:
+        print(f"Saving perlin to {args.perlin_save_path}...")
+        save_perlin_noise(args.perlin_save_path, 'p_x', p_x)
+        print("Saved p_x...")
+        save_perlin_noise(args.perlin_save_path, 'p_y', p_y)
+        print("Saved p_y...")
+        save_perlin_noise(args.perlin_save_path, 'p_z', p_z)
+        print("Saved p_z! Done!")
+
     v_f = VelocityField(p_x, p_y, p_z)
 
     no_bugs = 10
     bugs = []
     for _ in range(no_bugs):
-        x = random.randint(50, 60)
-        y = random.randint(50, 60)
-        z = random.randint(50, 60)
+        x = random.randint(100, 140)
+        y = random.randint(100, 140)
+        z = random.randint(100, 140)
         bugs.append(Insect((x,y,z), bound))
 
+    # save_images_folder_obj = tempfile.TemporaryDirectory()
+    # save_images_folder = save_images_folder_obj.name
+    save_images_folder = 'images'
+    if not os.path.isdir(save_images_folder):
+        os.mkdir(save_images_folder)
+
+    noise_gain = 200.0
+    no_of_numbers = len(str(no_frames))
+    frame_counter = 0
     for frame in range(no_frames):
+        frame = np.zeros((bound,bound,bound))
+        print("Moving bugs...", end='')
         for bug in bugs:
             # Print buggy
             x,y,z = bug.position
-            frames[frame, x, y, z] = 1
+            frame[x, y, z] = 1
             # Move buggy
             move_x, move_y, move_z = v_f.get_velocity(bug.position)
-            print(f"Float: m_x:{move_x} m_y:{move_y} m_z:{move_z}")
-            move_x = move_x * 100.0
-            move_y = move_y * 100.0
-            move_z = move_z * 100.0
-            print(f"With gain: m_x:{move_x} m_y:{move_y} m_z:{move_z}")
+            move_x = move_x * noise_gain
+            move_y = move_y * noise_gain
+            move_z = move_z * noise_gain
             move_x, move_y, move_z = v_f.round_velocity_vector((move_x, move_y, move_z))
-            print(f"Rounded: m_x:{move_x} m_y:{move_y} m_z:{move_z}")
             bug.move((move_x, move_y, move_z))
-        # frame is done!
-
-    save_video_from_grid(frames, 25, 'bugs_test_256.avi')
-
-def save_video_from_grid(grid, framerate, video_filename):
-    save_images_folder_obj = tempfile.TemporaryDirectory()
-    save_images_folder = save_images_folder_obj.name
-
-    no_of_numbers = len(str(grid.shape[0]))
-    no_of_frames = grid.shape[0]
-    for frame in range(no_of_frames):
-        x_vals, y_vals, z_vals = positions_from_grid(grid[frame])
-        filename = f"{save_images_folder}/bugs-frame-{frame:0>{no_of_numbers}}.png"
+        print("Generating frame...", end='')
+        x_vals, y_vals, z_vals = positions_from_grid(frame)
+        filename = f"{save_images_folder}/bugs-frame-{frame_counter:0>{no_of_numbers}}.png"
         save_image_from_grid(x_vals, y_vals, z_vals, filename=filename)
-        print(f"Saved frame {frame} as {filename}...")
+        print(f"Saved frame {frame_counter} as {filename}!\r", end='')
+        # frame is done!
+        frame_counter += 1
 
+    save_video_from_grid(save_images_folder, 25, args.output)
+
+    print(f"Cleaning up temporary folder {save_images_folder}...")
+    # save_images_folder_obj.cleanup()
+    print("Cleanup done.")
+
+def save_video_from_grid(frames_folder, framerate, video_filename):
     # generate video
     print("Using ffmpeg to generate avi video...")
     commands = [
@@ -104,24 +135,32 @@ def save_video_from_grid(grid, framerate, video_filename):
             '-pattern_type', # Regextype ...
             'glob', # ...set to global
             f"-i", # Pattern to use when ...
-            f"'{save_images_folder}/*.png'", # ...looking for image files
+            f"'{frames_folder}/*.png'", # ...looking for image files
             f"{video_filename}", # Where to save
             ]
     print(f"Running command '{' '.join(commands)}'")
     subprocess.run(' '.join(commands), shell=True)
     print("Dont generating video!")
-    # Clean up if we were using temporary folder for the images
-    print(f"Cleaning up temporary folder {save_images_folder}...")
-    save_images_folder_obj.cleanup()
-    print("Cleanup done.")
 
+def save_perlin_noise(folder, filename, p):
+    if not os.path.isdir(folder):
+        os.mkdir(folder)
+    with open(f'{folder}/{filename}.perlin', 'wb') as f:
+        pickle.dump(p, f)
+
+def load_perlin_noise(folder, filename):
+    loaded_p = None
+    with open(f'{folder}/{filename}.perlin', 'rb') as f:
+        loaded_p = pickle.load(f)
+    return loaded_p
 
 def save_image_from_grid(x_vals, y_vals, z_vals, elevation=30, xy_angle=-60, zoom=0, filename=None):
     dpi = 10
     side_size = 12.8
     # side_size = 25.6
     # side_size = 25.6
-    fig = plt.figure(figsize=(side_size, side_size), dpi=dpi)
+    # fig = plt.figure(figsize=(side_size, side_size), dpi=dpi)
+    fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.set_xlim(0,256)
     ax.set_ylim(0,256)
@@ -129,17 +168,20 @@ def save_image_from_grid(x_vals, y_vals, z_vals, elevation=30, xy_angle=-60, zoo
     # ax.plot(grid[:,0], grid[:,1], grid[:,2])
     # ax.plot(x_vals, y_vals, z_vals)
     # ax.scatter(x_vals, y_vals, z_vals)
-    ax.scatter(x_vals, y_vals, z_vals, c='white', depthshade=False)
+    # ax.scatter(x_vals, y_vals, z_vals, c='white', depthshade=False)
+    ax.scatter(x_vals, y_vals, z_vals, c='white', depthshade=True)
     ax.view_init(elev=elevation, azim=xy_angle)
     if zoom > 0:
         ax.margins(zoom, zoom, zoom)
-    else:
-        ax.margins(zoom)
+    # else:
+        # ax.margins(zoom)
     ax.grid(False)
     ax.axis('off')
     ax.set_facecolor('xkcd:black')
     fig.set_facecolor('xkcd:black')
-    plt.savefig(filename, dpi=dpi, edgecolor='xkcd:black', facecolor='xkcd:black')
+    # plt.savefig(filename, dpi=dpi, edgecolor='xkcd:black', facecolor='xkcd:black')
+    plt.savefig(filename, edgecolor='xkcd:black', facecolor='xkcd:black')
+    # plt.savefig(filename)
     # plt.savefig(filename, dpi=10)
     # plt.show()
     plt.close(fig)
